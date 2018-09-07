610947924cfda805c11b288d020683265f8d1477
tkalpana <43047499+tkalpana@users.noreply.github.com>
2018-09-07 15:44:43 +0530

    Add files via upload

---
+++ greaternum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    int num;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the number");
+    num=sc.nextInt();
+    if(num>=50)
+    {
+      System.out.println("greater than 50");
+    }
+    else
+    {
+      System.out.println("smaller than 50");
+    }
+   
+  }
+}
