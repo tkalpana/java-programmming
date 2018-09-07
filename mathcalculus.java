94d41a3947b95744c51855174dd01e99374d73da
tkalpana <43047499+tkalpana@users.noreply.github.com>
2018-09-07 11:19:47 +0530

    Add files via upload

---
+++ mathcalculus.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    float b,c,d,e,f;
+    a=15;
+    b=598.86f;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    System.out.println("the sum of two numbers:"+c);
+    System.out.println("the difference of two numbers:"+d);
+    System.out.println("the product of two numbers:"+e);
+    System.out.println("the division of two numbers:"+f);
+  }
+}
