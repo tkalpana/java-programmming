f320e351a47f962417d39b01e19216f5ef619612
tkalpana <43047499+tkalpana@users.noreply.github.com>
2018-10-05 12:32:29 +0530

    Add files via upload

---
+++ Alphabet or Not.java
@@ -0,0 +1,16 @@
+ import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    char ch='a';
+    if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z'))
+    System.out.println(ch+"is an Alphabet");
+    else
+    System.out.println(ch+"is not an Alphabet");
+  }
+}   
+    
+    
+    
+    
+    
+    
