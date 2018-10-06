7932b0abc8c18071f5b24ab8543b1d434928bfbc
tkalpana <43047499+tkalpana@users.noreply.github.com>
2018-10-06 12:34:18 +0530

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+  double n1=7,n2=8,n3=9;
+  if(n1>=n2&&n1>=n3)
+  System.out.println(n1+"is the largest number.");
+  else if(n2>=n1&&n2>n3)
+  System.out.println(n2+"is the largest number.");
+  else
+  System.out.println(n3+"is the largest number.");
+  }
+}
